88c825ad6615d13c2fd9acb816ece6f5c18ae0a9
Adam Taber <altaber@gmail.com>
2023-01-27 11:17:13 -0600

    uibuild

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Contact = require('./models/contact')
 
 const app = express()
 
@@ -8,104 +10,95 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 
-morgan.token('content', (req, res) => { 
-  return JSON.stringify(req.body) 
-})
+// morgan.token('content', (req, res) => { 
+//   return JSON.stringify(req.body) 
+// })
 
-app.use(morgan(':method :url :status :res[content-length] '
-              +'- :response-time ms :content'))
-
-let contacts = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+// app.use(morgan(':method :url :status :res[content-length] '
+//               +'- :response-time ms :content'))
 
 app.get('/info', (request,response) => {
-  response.send(
-    `<p>Phonebook has info for ${contacts.length} people</p>
-     <p>${new Date()}</p>`
-  )
+  Contact.find({}).then(contacts => {
+    response.send(
+     `<p>Phonebook has info for ${contacts.length} people</p>
+      <p>${new Date()}</p>`
+    )
+  })
 })
 
 app.get('/api/contacts', (request, response) => {
-  response.json(contacts)
+  Contact.find({}).then(contacts => {
+    response.json(contacts)
+  })
 })
 
-app.get('/api/contacts/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const contact = contacts.find(contact => contact.id === id)
-  
-  if (contact) {
-    response.json(contact)
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/contacts/:id', (request, response, next) => {
+  Contact.findById(request.params.id)
+    .then(contact => {
+      if(contact) {
+        response.json(contact)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/contacts/:id', (request, response) => {
-  const id = Number(request.params.id)
-  contacts = contacts.filter(contact => contact.id !== id)
-
-  response.status(204).end()
-})
-
-const generateId = () => {
-  const newId = Math.floor(Math.random() * 1000)
-  const currentIds = contacts.map(contact => contact.id)
-
-  if (currentIds.includes(newId)) {
-    generateId()
-  } else {
-    return newId
-  }
-}
-
-app.post('/api/contacts', (request, response) => {
+app.put('/api/contacts/:id', (request, response, next) => {
   const body = request.body
 
-  console.log(body)
-
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: 'info missing'
-    })
-  }
-
-  if (contacts.find(contact => contact.name === body.name)) {
-    return response.status(400).json({
-      error: 'this person is already in your contacts'
-    })
-  }
-
   const contact = {
-    id: generateId(),
     name: body.name,
-    number: body.number,
+    number: body.number
   }
 
-  contacts = contacts.concat(contact)
-
-  response.json(contact)
+  Contact.findByIdAndUpdate(request.params.id, contact, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
+app.post('/api/contacts', (request, response, next) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const contact = new Contact({
+    name: body.name,
+    number: body.number
+  })
+
+  contact.save()
+    .then(savedContact => {
+      response.json(savedContact)
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/contacts/:id', (request, response, next) => {
+  Contact.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).send('User has been deleted')
+    })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
